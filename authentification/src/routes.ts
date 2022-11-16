45e5425e0129e70202e4dd455034153e087c7ec4
remiCzn <cazinremi2@gmail.com>
2022-11-16 11:38:06 +0100

    Ajout routes

--- authentification/src/routes.ts
+++ authentification/src/routes.ts
@@ -1,5 +1,11 @@
 import * as express from "express";
 
 export const register = (app: express.Application) => {
-    app.get('/', (req,res) => res.send('Hello World'));
+    app.get('/', (req,res) => res.send('<h1>Hello World</h1>'));
+
+    app.get('/login', (req, res) => {});
+    
+    app.get('/register', (req, res) => {});
+
+    app.get('/usersLogin', (req, res) => {});
 }
