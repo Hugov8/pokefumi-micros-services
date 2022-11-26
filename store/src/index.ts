7c867c30c295c006f17674385e99ca66b1574c78
Hugo <hugovedrine@hotmail.com>
2022-11-26 15:03:22 +0100

    Modification des ports + ajout dans docker-compose

--- store/src/index.ts
+++ store/src/index.ts
@@ -1,7 +1,7 @@
 import {app} from "./app";
 import {AddressInfo} from 'net';
 
-const server = app.listen(5002, '0.0.0.0', () => {
+const server = app.listen(5004, '0.0.0.0', () => {
     const {port, address} = server.address() as AddressInfo;
     console.log("Server listenng on:", "http://"+address+":"+port);
 })
